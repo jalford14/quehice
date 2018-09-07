e1bfb5c72212cb730841e37fcbd928289448a7c1
Jimmy Alford <jalford14@liberty.edu>
2018-09-07 17:16:36 -0400

    Can create a new task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,19 @@
 class TasksController < ApplicationController
+    protect_from_forgery
+
     def index
         @tasks = Task.all
+    end
+
+    def new
+    end
+
+    def create
+        @task = Task.new(params.require(:task).permit(:description, :date))
+
+        @task.save
+    end
+    
+    def show
     end
 end
