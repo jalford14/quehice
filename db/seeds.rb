8b5cca3bbde76bf9c60caafe5196892a5c73ffa4
Jimmy Alford <jalford14@liberty.edu>
2018-09-07 09:36:31 -0400

    Initial commit of Task model.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Task.create(date: 'January 1, 1989', description: 'Feed the dog')
+Task.create(date: 'March 16, 2017', description: 'Take out trash')
+Task.create(date: 'September 1, 2017', description: 'Make lunch')
+Task.create(date: 'January 17, 2017', description: 'Pick up kids')
+Task.create(date: 'April 1, 2014', description: 'Finish homework')
+Task.create(date: 'April 30, 1996', description: 'Read new book')
+Task.create(date: 'January 06, 2018', description: 'Run')
+Task.create(date: 'June 2, 2018', description: 'Get groceries')
