3338d58dbc1267610991db668ec645ab84ddcf4c
Jimmy Alford <jalford14@liberty.edu>
2018-09-07 17:39:28 -0400

    path to create a new entry

--- app/javascript/components/Tasks/newEntry.jsx
+++ app/javascript/components/Tasks/newEntry.jsx
@@ -5,7 +5,7 @@ import React from 'react';
 class NewEntry extends React.Component {
   render() {
     return(
-      <a href = "#">
+      <a href = "/tasks/new">
 
         <div class = "task_box">
        
