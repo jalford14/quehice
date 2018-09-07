d8289b4133a5b24d2bce99ba778fa95f8f82fdfe
Jimmy Alford <jalford14@liberty.edu>
2018-09-07 09:34:08 -0400

    Welcome page along with History and Tasks buttons.

---
+++ app/javascript/components/Welcome.jsx
@@ -0,0 +1,25 @@
+import React from "react"
+import PropTypes from "prop-types"
+
+// Components
+import Tasks from "./Welcome/tasks";
+import History from "./Welcome/history";
+
+class Welcome extends React.Component {
+  render () {
+    return (
+      <React.Fragment>
+        <h1 id = "title">&#191;Quehice?</h1>
+
+        <div id = "container">
+          <Tasks />
+          <History />
+        </div>
+        
+      </React.Fragment>
+      
+    );
+  }
+}
+
+export default Welcome
---
+++ app/javascript/components/Welcome/history.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+class History extends React.Component {
+  render() {
+    return(
+        <a href = "#">
+            <div class = "boxes">
+                History
+            </div>
+        </a>
+    )
+  }
+}
+export default History
