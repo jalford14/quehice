c45f17c06d4c01ce294658ac91501163aa801538
Jimmy Alford <jalford14@liberty.edu>
2018-09-29 12:00:23 -0400

    Gets a valid date for entry

--- app/javascript/components/Tasks/New/createTask.jsx
+++ app/javascript/components/Tasks/New/createTask.jsx
@@ -3,17 +3,27 @@
 import React from 'react';
 
 class NewEntry extends React.Component {
-    constructor() {
-        super();
+    // Gets a valid date for entry
+    getDate() {
         var today = new Date();
+        var year = today.getFullYear();
+        var month = today.getMonth() + 1;
+        var day = today.getDate();
         
+        if(day < 10) { day = '0' + day; } 
+        
+        if(month < 10) { month = '0' + month; } 
+        
+        var date = year + '' + month + '' + day;
+        return date;
     }
+
   render() {
     return(
             <div id="description">
                 <form action="/tasks" method="post">
                     <input type='hidden' name='authenticity_token' value={this.props.authenticity_token} />
-                    <input type="hidden" name="task[date]" value={this.getDate} />
+                    <input type="hidden" name="task[date]" value={this.getDate()} />
                     <input type="text" name="task[description]" autocomplete="off" autofocus="true"/>
                 </form>
             </div>
