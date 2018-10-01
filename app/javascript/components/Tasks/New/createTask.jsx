b1e64c4987cc669df0b67a683dc8335a5ded1e66
Jimmy Alford <jalford14@liberty.edu>
2018-10-01 13:34:19 -0400

    Fixed issue with creating task.

--- app/javascript/components/Tasks/New/createTask.jsx
+++ app/javascript/components/Tasks/New/createTask.jsx
@@ -8,7 +8,7 @@ class NewEntry extends React.Component {
         this.state = {
             value: props.value || '',
             method: props.method || '',
-            action: props.action || '/tasks'
+            action: props.action || "/tasks"
         };
 
         this.handleChange = this.handleChange.bind(this);
@@ -33,11 +33,12 @@ class NewEntry extends React.Component {
     }
 
   render() {
+      console.log(this.props)
     return(
             <div id="description">
-                <form action={this.props.action} method="post">
+                <form action={this.state.action} method="post">
                     <input type="hidden" name='authenticity_token' value={this.props.authenticity_token} />
-                    <input type="hidden" name="_method" value={this.props.method} />
+                    <input type="hidden" name="_method" value={this.state.method} />
                     <input type="hidden" name="task[date]" value={this.getDate()} />
                     <input type="text" value={this.state.value} name="task[description]" autocomplete="off" autofocus="true" onChange={this.handleChange}/>
                 </form>
